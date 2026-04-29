b0965a278445650ae349d0dead6e8e540946e10c
Aryan Sanjay Nagbanshi <aryan.s.nag@gmail.com>
2026-04-29 22:30:54 +0200

    chore : backend refactor

--- worker/index.ts
+++ worker/index.ts
@@ -4,26 +4,6 @@ export { YcalidrawWebSocketServer } from "./durable-objects";
 
 const app = new Hono<{ Bindings: Env }>();
 
-//app.get("/new/:roomId", async (c) => {
-//  //get a roomId or room name to connect to the right durable object
-//  const roomId = c.req.param("roomId");
-//  if (!roomId) {
-//    return c.json(
-//      {
-//        message: "Please enter a room id or create a new room.",
-//      },
-//      400,
-//    );
-//  }
-//  // A stub is a client Object used to send messages to the Durable Object.
-//  //@ts-ignore
-//  let stub = c.env.DURABLE_OBJECT.getByName(roomId);
-//  let count = null;
-//  count = await stub.increment();
-//  let name = count.toString();
-
-//  return c.json({ name: name });
-//});
 
 app.get("/api/ws/:drawingId", async (c) => {
   const drawingId = c.req.param("drawingId");
